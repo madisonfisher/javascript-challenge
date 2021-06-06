12b89a6f0d37a367916bce74fc884ce888fc52fe
madisonfisher <mbfishe2@ncsu.edu>
2021-06-06 13:47:43 -0400

    filter the table by city, date, and shape

--- UFO-level-2/index.js
+++ UFO-level-2/index.js
@@ -7,8 +7,6 @@ var dateSelected = d3.select("#date");
 //reference to the table
 var tbody = d3.select("tbody");
 
-
-
 button.on("click", function () {
     //preventing refresh
     d3.event.preventDefault();
@@ -23,17 +21,37 @@ button.on("click", function () {
     var inputCityValue = citySelect.property("value");
     var inputCity = inputCityValue.toLowerCase();
 
-    //creating date and city filter
-    if (inputCity === "" || inputDate === "") {
-        function filterBy(sighting) {
-            return sighting.datetime === inputDate || sighting.city === inputCity;}}
-    else {
-            function filterBy(sighting) {
-        return sighting.datetime === inputDate && sighting.city === inputCity;
-    }}
+    //reference to the shape select
+    var shapeSelect = d3.select("#shape");
+    //getting shape as value
+    var inputShape = shapeSelect.property("value");
 
-    //then filtering by city
-    var filtedSightings = data.filter(filterBy);
+    function filterByDate(sighting) {
+        return sighting.datetime === inputDate 
+        }
+    function filterByCity(sighting) {
+        return sighting.city === inputCity
+        }
+    function filterByShape(sighting) {
+        return sighting.shape === inputShape 
+        }
+
+    if (inputDate === "") {
+        var filtedSightings1 = data.filter(filterByCity);
+        var filtedSightings = filtedSightings1.filter(filterByShape);
+    }
+    else if (inputCity === "") {
+        var filtedSightings1 = data.filter(filterByDate);
+        var filtedSightings = filtedSightings1.filter(filterByShape);
+    }
+    else if (inputCity === "" && inputDate === "") {
+        var filtedSightings = data.filter(filterByShape);
+    }
+    else {
+        var filtedSightings1 = data.filter(filterByDate);
+        var filtedSightings2 = filtedSightings1.filter(filterByCity);
+        var filtedSightings = filtedSightings2.filter(filterByShape);
+    }
 
     //removed original table to allow for only filtered rows to show up
     tbody.html("");
